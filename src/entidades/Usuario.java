d96dae0b26cc5f1872f44756d46fe9e8c0836468
ivanbs5 <99034875+ivanbs5@users.noreply.github.com>
2024-04-22 13:06:53 +0200

    Pruebas

--- src/entidades/Usuario.java
+++ src/entidades/Usuario.java
@@ -1,60 +1,60 @@
 package entidades;
 
 public class Usuario {
-//hola
-private int id;
-private String nombre;
-private String email;
-private String password;
-private String tipo;
 
-public Usuario(int id, String nombre, String email, String password, String tipo) {
-	super();
-	this.id = id;
-	this.nombre = nombre;
-	this.email = email;
-	this.password = password;
-	this.tipo = tipo;
-}
+	private int id;
+	private String nombre;
+	private String email;
+	private String password;
+	private String tipo;
 
-public int getId() {
-	return id;
-}
+	public Usuario(int id, String nombre, String email, String password, String tipo) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.email = email;
+		this.password = password;
+		this.tipo = tipo;
+	}
 
-public void setId(int id) {
-	this.id = id;
-}
+	public int getId() {
+		return id;
+	}
 
-public String getNombre() {
-	return nombre;
-}
+	public void setId(int id) {
+		this.id = id;
+	}
 
-public void setNombre(String nombre) {
-	this.nombre = nombre;
-}
+	public String getNombre() {
+		return nombre;
+	}
 
-public String getEmail() {
-	return email;
-}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-public void setEmail(String email) {
-	this.email = email;
-}
+	public String getEmail() {
+		return email;
+	}
 
-public String getPassword() {
-	return password;
-}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-public void setPassword(String password) {
-	this.password = password;
-}
+	public String getPassword() {
+		return password;
+	}
 
-public String getTipo() {
-	return tipo;
-}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-public void setTipo(String tipo) {
-	this.tipo = tipo;
-}
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
 
 }
